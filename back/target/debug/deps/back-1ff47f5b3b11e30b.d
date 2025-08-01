679e907f053a81f9767cb632ba0a5138a4867723
Mr_Tomate <mr_tomate2024@hotmail.com>
2025-07-31 20:17:05 -0400

    Exclusão pasta target Rust

--- back/target/debug/deps/back-1ff47f5b3b11e30b.d
+++
@@ -1,5 +0,0 @@
-/home/mr_tomate/LixeiraDev/Rust/blog/back/target/debug/deps/back-1ff47f5b3b11e30b.d: src/main.rs
-
-/home/mr_tomate/LixeiraDev/Rust/blog/back/target/debug/deps/libback-1ff47f5b3b11e30b.rmeta: src/main.rs
-
-src/main.rs:
