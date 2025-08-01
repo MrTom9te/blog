c2b2443342f9422ca71df1a9543acf5be62e3b1d
Mr_Tomate <mr_tomate2024@hotmail.com>
2025-07-31 20:13:29 -0400

    Meu Blog porra

---
+++ back/target/debug/deps/back-6090c589869a7f3c.d
@@ -0,0 +1,5 @@
+/home/mr_tomate/LixeiraDev/Rust/blog/back/target/debug/deps/back-6090c589869a7f3c.d: src/main.rs
+
+/home/mr_tomate/LixeiraDev/Rust/blog/back/target/debug/deps/libback-6090c589869a7f3c.rmeta: src/main.rs
+
+src/main.rs:
